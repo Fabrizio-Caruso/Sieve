78c12ed8970816aa573544497fb49864b072f9f9
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2019-07-09 23:57:55 +0200

    tab

--- sieve_compressed.c
+++ sieve_compressed.c
@@ -12,47 +12,47 @@ int main(void)
 {
     clock_t Ticks, TicksDelta;
     unsigned int Sec;
-    unsigned int Milli;	
-	
+    unsigned int Milli; 
+    
     unsigned int i = 3;
     unsigned int j = 0;
 
-	for(j=0;j<SIEVE_SIZE;++j)
-	{
-		composite[j]=0;
-	}
-	
+    for(j=0;j<SIEVE_SIZE;++j)
+    {
+        composite[j]=0;
+    }
+    
     printf("\nPress enter to start\n");
     getchar();
     printf("\nComputing...\n"); 
-	Ticks = clock();
+    Ticks = clock();
     while(i<2*SIEVE_SIZE)
     {
-		// getchar();
-		// printf("i : %u is prime; index at %u\n", i, i>>1);
+        // getchar();
+        // printf("i : %u is prime; index at %u\n", i, i>>1);
         for(j=i*2;j<2*SIEVE_SIZE;j+=i)
         {
             if(j&1)
-			{
-				// printf("j: %u is composite; index at %u\n", j, j>>1);
-				composite[j>>1]=1;
-			}
+            {
+                // printf("j: %u is composite; index at %u\n", j, j>>1);
+                composite[j>>1]=1;
+            }
         }
-		i+=1;
+        i+=1;
         while(composite[i>>1])
         {
-			i+=1;
+            i+=1;
         }
-		if(!(i&1))
-		{
-			++i;
-		}
+        if(!(i&1))
+        {
+            ++i;
+        }
     }
     TicksDelta = clock() - Ticks;
     Sec = (unsigned short) (TicksDelta / CLOCKS_PER_SEC);
     Milli = ((TicksDelta % CLOCKS_PER_SEC) * 1000) / CLOCKS_PER_SEC;
     printf ("Time used: %u.%03u seconds = %u ticks\n", Sec, Milli, (unsigned short) TicksDelta);    
-     	
+        
     printf("Press enter to print the prime numbers\n");
     getchar(); 
     printf("\n");
